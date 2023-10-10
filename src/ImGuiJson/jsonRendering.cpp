a0e7714cb2ebd660ccfbb66161e30cc4b9160ffa
Dannode36 <crafty_dan@outlook.com>
2023-10-10 15:24:20 +1100

    JSON Rendering

--- src/ImGuiJson/jsonRendering.cpp
+++ src/ImGuiJson/jsonRendering.cpp
@@ -1,21 +1,54 @@
 #include "jsonRendering.h"
+#include <string>
 #include "imgui.h"
-#include "rapidjson/pointer.h"
 
+//Base Types
 void jsonDragInt(const char* label, rapidjson::Value* jsonValue, int* imguiValue) {
 	if (ImGui::DragInt(label, imguiValue)) {
 		jsonValue->SetInt(*imguiValue);
 	}
 }
-
 void jsonDragFloat(const char* label, rapidjson::Value* jsonValue, float* imguiValue) {
 	if (ImGui::DragFloat(label, imguiValue)) {
 		jsonValue->SetFloat(*imguiValue);
 	}
 }
+void jsonInputText(const char* label, rapidjson::Value* jsonValue, char* buf) {
+	if (ImGui::InputText(label, buf, strlen(buf))) {
+		jsonValue->SetString(buf, strlen(buf));
+	}
+}
 
-void jsonInputText(const char* label, rapidjson::Value* jsonValue, char* imguiValue) {
-	if (ImGui::InputText(label, imguiValue, strlen(imguiValue))) {
-		jsonValue->SetString(imguiValue, strlen(imguiValue));
+//Array Types
+void jsonDragIntA(const char* label, rapidjson::Value* jsonValue, int* imguiArray) {
+	if (jsonValue->Empty()) { return; }
+	assert(jsonValue[0].IsInt());
+	for (rapidjson::SizeType i = 0; i < jsonValue->Size(); i++)
+	{
+		std::string elemLabel(label);
+		elemLabel += std::to_string(i);
+		jsonDragInt(elemLabel.c_str(), &jsonValue[i], &imguiArray[i]);
+	}
+}
+
+void jsonDragFloatA(const char* label, rapidjson::Value* jsonValue, float* imguiArray) {
+	if (jsonValue->Empty()) { return; }
+	assert(jsonValue[0].IsFloat());
+	for (rapidjson::SizeType i = 0; i < jsonValue->Size(); i++)
+	{
+		std::string elemLabel(label);
+		elemLabel += std::to_string(i);
+		jsonDragFloat(elemLabel.c_str(), &jsonValue[i], &imguiArray[i]);
+	}
+}
+
+void jsonInputTextA(const char* label, rapidjson::Value* jsonValue, char** imguiArray) {
+	if (jsonValue->Empty()) { return; }
+	assert(jsonValue[0].IsFloat());
+	for (rapidjson::SizeType i = 0; i < jsonValue->Size(); i++)
+	{
+		std::string elemLabel(label);
+		elemLabel += std::to_string(i);
+		jsonInputText(elemLabel.c_str(), &jsonValue[i], imguiArray[i]);
 	}
 }
--- src/ImGuiJson/jsonRendering.h
+++ src/ImGuiJson/jsonRendering.h
@@ -1,1 +1,8 @@
 #pragma once
+#include "rapidjson/pointer.h"
+
+void jsonDragInt(const char* label, rapidjson::Value* jsonValue, int* imguiValue);
+void jsonDragFloat(const char* label, rapidjson::Value* jsonValue, float* imguiValue);
+void jsonInputText(const char* label, rapidjson::Value* jsonValue, char* imguiValue);
+
+void jsonDragIntA(const char* label, rapidjson::Value* jsonValue, int* imguiArray);
